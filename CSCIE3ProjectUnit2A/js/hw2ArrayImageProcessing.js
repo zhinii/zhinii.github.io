93773c5a468a7417718de2a44f434e76e1acb1d4
zhinii <hansen.c.ralph@gmail.com>
2016-10-12 16:58:26 -0400

    Update hw2ArrayImageProcessing.js

--- CSCIE3ProjectUnit2A/js/hw2ArrayImageProcessing.js
+++ CSCIE3ProjectUnit2A/js/hw2ArrayImageProcessing.js
@@ -89,7 +89,7 @@ function makeReverse(original, output){
 
           
  for (var i = 0; i < original.length; i++){
-         if ([i]%4 < 3){output[i] = 255-output[i];}
+         if ([i]%4 < 3){output[i] = 255-original[i];}
           // console.log(copy);
       // return output;
     else {output[i]=original[i];}
