02f32eafff909753e1972e628e206408ae13ef93
zhinii <hansen.c.ralph@gmail.com>
2016-10-12 16:49:26 -0400

    Update hw2ArrayImageProcessing.js

--- CSCIE3ProjectUnit2A/js/hw2ArrayImageProcessing.js
+++ CSCIE3ProjectUnit2A/js/hw2ArrayImageProcessing.js
@@ -98,6 +98,106 @@ function makeReverse(original, output){
 // output = copy;
 console.log(output);
       return output;
+/* CSCI E-3 Introduction to Web Programming Using Javascript
+ *
+ *
+ * Homework Unit #2a
+ *
+ *
+ */
+
+ /********************************************************************
+  *
+  * Image processing by way of arrays:  This assignment is designed to
+  * give you a chance to work with arrays. The fact that we're processing images
+  * makes the example interesting, but no prior knowledge of image processing
+  * or understanding of the setup for this in hw2ArrayImageProcessingSetup.js
+  * is required (though you're welcome to study that if you like!).
+  *
+  * In each of these functions, you'll be reading the parameter 'original',
+  * which is an array of pixel data.  Each array contains four numeric elements to
+  * describe each pixel in the image (red, green, blue, alpha).  The
+  * data looks like this:
+  *   original[0];  // pixel 0 red value
+  *   original[1];  // pixel 0 green value
+  *   original[2];  // pixel 0 blue value
+  *   original[3];  // pixel 0 alpha value
+  *   original[4];  // pixel 1 red value
+  *   original[5];  // pixel 1 green value
+  *   original[6];  // pixel 1 blue value
+  *   original[7];  // pixel 1 alpha value
+  *     etc...
+  *
+  * Essentially, your job is to read
+  * data from the original array, and copy it to the output array, making
+  * certain modifications along the way. It might be a good idea to start by
+  * iterating over the original array and copying its data unmodified into the
+  * output array. Once you have that working, you can try the data changes
+  * required to make the output correct.
+  *
+  ********************************************************************/
+
+
+
+/*
+ * makeBlue - Reads data from an image bitmap array and writes new image data to another array object
+ *            The output data should contain only blue pixel data, with other red and green color values set to 0.
+ *
+ *            @original {array} - the source bitmap data, an array of integers from 0-255
+ *                         each pixel is represented by four consecutive array elements (r, g, b, a)
+ *                         (red, green, blue, alpha), so the array has 4x elements as the image has pixels
+ *            @output {array} - the bitmap data array to which the output image is written.
+ *
+ **/
+function makeBlue(original, output){
+      // YOUR CODE GOES HERE
+//     output=original
+          console.log(output)
+          console.log(original)
+
+ for (var i = 0; i < original.length; i++){
+         if ([i]%4 < 2){output[i] = 0;}
+          // console.log(copy);
+      // return output;
+    else {output[i]=original[i];}
+      }
+
+// output = copy;
+console.log(output);
+      return output;
+}
+
+
+
+/*
+ * makeReverse - Reads data from an image bitmap array and writes new image data to another array object
+ *               The output data contains pixel data inverted, with every color value its opposite on the scale of 0-255.
+ *               To get "inverted" color, the value for each color will be 255-n, where n is the original color.
+ *               Don't invert the alpha value - that's opacity, and if you invert it, your picture will be trasparent (invisible!)
+ *
+ *            @original {array} - the source bitmap data, an array of integers from 0-255
+ *                         each pixel is represented by four consecutive array elements (r, g, b, a)
+ *                         (red, green, blue, alpha), so the array has 4x elements as the image has pixels
+ *            @output {array} - the bitmap data array to which the output image is written.
+ *
+ **/
+function makeReverse(original, output){
+      // YOUR CODE GOES HERE
+ //     output=original
+          console.log(output)
+          console.log(original)
+
+          
+ for (var i = 0; i < original.length; i++){
+         if ([i]%4 < 3){output[i] = 255-original[i];}
+          // console.log(copy);
+      // return output;
+    else {output[i]=original[i];}
+      }
+
+// output = copy;
+console.log(output);
+      return output;
 }
 
 /*
@@ -114,21 +214,6 @@ console.log(output);
 
 function makeTransparent(original,output){
       // YOUR CODE GOES HERE
-         console.log(output)
-          console.log(original)
-
-          
- for (var i = 0; i < original.length; i++){
-         if ([i]%4 = 0){output[i] = original[i]/2;}
-          // console.log(copy);
-      // return output;
-    else {output[i]=original[i];}
-      }
-
-// output = copy;
-console.log(output);
-      return output;
-}
 }
 
 /*
