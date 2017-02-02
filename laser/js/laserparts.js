2ab1d1836913db8f9afc25a32b88c88ea3ac48e5
zhinii <hansen.c.ralph@gmail.com>
2017-02-02 16:08:59 -0500

    Update laserparts.js

--- laser/js/laserparts.js
+++ laser/js/laserparts.js
@@ -1,12 +1,53 @@
-var leadscrew = new THREE.MTLLoader();
+//zaxis parts
+
+var Zpulley = new THREE.MTLLoader();
 			
-			lasermodelSolid.load('models/leadscrew.mtl', function(material){
+			lasermodelSolid.load('models/Zpulley.mtl', function(material){
 				material.preload();
 				var loader = new THREE.OBJLoader();
 				loader.setMaterials(material);
-				loader.load('models/leadscrew.obj', function(object){
+				loader.load('models/Zpulley.obj', function(object){
 					scene.add(object);
 				
 				});
 				
 			});
+
+
+var Zbelt = new THREE.MTLLoader();
+			
+			lasermodelSolid.load('models/Zbelt.mtl', function(material){
+				material.preload();
+				var loader = new THREE.OBJLoader();
+				loader.setMaterials(material);
+				loader.load('models/Zbelt.obj', function(object){
+					scene.add(object);
+				
+				});
+				
+			});
+
+
+var Zscrew = new THREE.MTLLoader();
+			
+			lasermodelSolid.load('models/Zscrew.mtl', function(material){
+				material.preload();
+				var loader = new THREE.OBJLoader();
+				loader.setMaterials(material);
+				loader.load('models/Zscrew.obj', function(object){
+					scene.add(object);
+				
+				});
+				
+			});
+
+	var Zparts = [Zscrew, Zpulley, Zbelt];
+
+
+
+var zClick = document.getElementById('zscrews');
+zClick.addEventListener('mouseon', zHover);
+
+function mouseon(){
+	console.log('hi');
+}
