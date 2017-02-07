402e694c5617050d5ac90aed449ad5f9e7082d56
zhinii <hansen.c.ralph@gmail.com>
2017-02-07 18:38:31 -0500

    Update laserparts.js

--- laser/js/laserparts.js
+++ laser/js/laserparts.js
@@ -1,28 +1,28 @@
-var intersectColor = 0xff0000;
-	var Zparts = [];
-//zaxis parts
-var red = new THREE.MeshLambertMaterial({
-	color: 0xff0000
-});
-var parts = [];
-var loader = new THREE.OBJLoader();
-
-loader.load('models/Zbelt.obj', function(object){
-	object.traverse( function ( child ) {
-        if ( child instanceof THREE.Mesh )
-            child.material.color.setHex (0xffffff);
-		parts.push(child);
-    });
-	scene.add(object);
-});
-
-
 
 var zClick = document.getElementById('zscrews');
 zClick.addEventListener('mouseover', zHover);
 
+
+var intersectColor = 0xff0000;
+	
+//zaxis parts
+
+var parts = [];
+var loader = new THREE.OBJLoader();
+
+loader.load('models/Zbelt.obj', function(object){
+	
 function zHover(){
 	console.log('ok');
-	parts[0].material.color.setHex(0xff0000);
+	object.traverse( function ( child ) {
+        if ( child instanceof THREE.Mesh )
+            child.material.color.setHex (0xff0000);
+    });
 }
+
+
+	scene.add(object);
+	parts.push(object);
+});
+
 
