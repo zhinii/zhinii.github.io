e49ae7a2d79e78739e677b9348d50f11811327ec
zhinii <hansen.c.ralph@gmail.com>
2017-02-07 16:41:21 -0500

    Update laserparts.js

--- laser/js/laserparts.js
+++ laser/js/laserparts.js
@@ -8,7 +8,9 @@ var red = new THREE.MeshLambertMaterial({
 var loader = new THREE.OBJLoader();
 
 loader.load('models/Zbelt.obj', function(geometry){
+	geometry.traverse(function(child){
 	geometry.material.color.setHex(0xff0000);
+	}
 	scene.add(geometry);
 });
 
