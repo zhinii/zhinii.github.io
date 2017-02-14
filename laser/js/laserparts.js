cadd1ac18c886fc24bd0c3d3dc7824e39a9e0328
zhinii <hansen.c.ralph@gmail.com>
2017-02-14 16:20:36 -0500

    Update laserparts.js

--- laser/js/laserparts.js
+++ laser/js/laserparts.js
@@ -2,7 +2,16 @@
 var baseColor = 0xffffff;
 
 var partHilite = ['#motor', '#zbelt', '#zscrews'];
-	
+
+for(var i=0; i< partHilite.length; i++) {
+       partHilite[i].addEventListener("click", bindClick(i));
+ }
+
+ function bindClick(i) {
+    return function(){
+             console.log("you clicked region number " + i);
+           });
+ }	
 
 
 	
@@ -18,25 +27,25 @@ loader.load('models/Zbelt.obj', function(object){
     });
 	scene.add(object);
 
-for(var i=0; i< partHilite.length; i++) {
-       partHilite[i].addEventListener("click", function(){
-			if (bindClick[i] = '#zbelt')
-				{object.traverse(function(child)
-					{if(child.material)
-						{child.material.color.setHex(0xff0000);}
-							});
-			}
-			else{
-			object.traverse(function(child){
-			if(child.material){
-				child.material.color.setHex(0xffffff);
-			}
-		});
-		}
-	});
-			//
+// for(var i=0; i< partHilite.length; i++) {
+//        partHilite[i].addEventListener("click", function(){
+// 			if (bindClick[i] = '#zbelt')
+// 				{object.traverse(function(child)
+// 					{if(child.material)
+// 						{child.material.color.setHex(0xff0000);}
+// 							});
+// 			}
+// 			else{
+// 			object.traverse(function(child){
+// 			if(child.material){
+// 				child.material.color.setHex(0xffffff);
+// 			}
+// 		});
+// 		}
+// 	});
+// 			
 		
-}
+// }
 });
 
 
