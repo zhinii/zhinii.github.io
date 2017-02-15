a352883294e166f65ea90b1633e06211f4297f63
zhinii <hansen.c.ralph@gmail.com>
2017-02-15 16:19:06 -0500

    Update laserparts.js

--- laser/js/laserparts.js
+++ laser/js/laserparts.js
@@ -1,46 +1,34 @@
-
 var baseColor = 0xffffff;
 
 var menuClick = document.getElementById('menu');
 
 
-	
+
 //zaxis parts
 
 var loader = new THREE.OBJLoader();
 
-loader.load('models/Zbelt.obj', function(object){
-
-	object.traverse( function ( child ) {
-        if ( child instanceof THREE.Mesh )
-            child.material.color.setHex (baseColor);
+loader.load('models/Zbelt.obj', function(object) {
+    object.traverse(function(child) {
+        if (child instanceof THREE.Mesh)
+            child.material.color.setHex(baseColor);
     });
-	scene.add(object);
+    scene.add(object);
 
-
-
-var thing = document.getElementById('zbelt');
-
-menuClick.addEventListener('click', function(){
-	if(event.target == thing){
-	
-			object.traverse(function(child)
-					{if(child.material)
-						{child.material.color.setHex(0xff0000);}
-							});
-	}
-	else{
-		object.traverse(function(child){
-			if(child.material){
-				child.material.color.setHex(0xffffff);
-	}
+    var thing = document.getElementById('zbelt');
+    menuClick.addEventListener('click', function() {
+        if (event.target == thing) {
+            object.traverse(function(child) {
+                if (child.material) {
+                    child.material.color.setHex(0xff0000);
+                }
+            });
+        } else {
+            object.traverse(function(child) {
+                if (child.material) {
+                    child.material.color.setHex(0xffffff);
+                }
+            });
+        }
+    });
 });
-	}
-});
-
-
-});
-
-
-
-
