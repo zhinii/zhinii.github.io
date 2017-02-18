d9921db1066f939b7ab67c80d5d4592603e6d3c7
zhinii <hansen.c.ralph@gmail.com>
2017-02-17 21:11:02 -0500

    updates

--- p5/ass1/sketch/sketch.js
+++ p5/ass1/sketch/sketch.js
@@ -2,25 +2,57 @@
 ///Homage to the Square: Against Deep Blue
 function setup() {
 
-  //canvas size 800 x 800
+  //set canvas size
     createCanvas(1024 , 768);
+    //set bg color
     background('blue');
+    //create circle embedded in background
+    fill('black');
+    ellipse(1024/2, 768/2, 600, 600)
 }
+
+//mouse click event that clears background and changes its color
+function mousePressed(){
+  clear();
+
+    background('blue');
+//changes the black circle in the background to multiple black circles arrayed accross canvas
+for(var x = 0; x<1024; x = x+100){
+  ellipse(x, 768/2, 100, 100);
+}
+}
+
+//draw objects
+
 
 function draw() {
   noStroke();
-
-  
+// var to store mouse position
+var xcoord = mouseX;
+var ycoord = mouseY;
+//draw of first square
   push();
- fill('orange');
-  rect(100, 125, 150, 150);
+  //mouse position determines color using if else statement
+  if(mouseX < 500 ){
+ fill('orange');}
+ else{
+   fill('red')
+ }
+ //mouse position determines rect position
+  rect(xcoord, ycoord, 150, 150);
   pop();
-  
+  //code from above is repeated for for a smaller rectangle
    push();
-   fill('red');
-  rect(175, 175,50,50);
+    if(mouseX < 500 ){
+ fill('red');}
+ else{
+   fill('orange');
+ }
+  rect(xcoord + 50, ycoord + 25,50,50);
   pop();
-  
+
+  //these create a grey border around the canvas.
+
   push()
   noFill();
   strokeWeight(15);
