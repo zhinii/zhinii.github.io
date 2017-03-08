f4e100c9bd67298eb4dfa92120f132b62da789fa
zhinii <hansen.c.ralph@gmail.com>
2017-03-08 16:39:14 -0500

    Update laserparts.js

--- laser/js/laserparts.js
+++ laser/js/laserparts.js
@@ -1,20 +1,20 @@
-var baseColor = 0xffffff;
+
+var meshBaseColor = new THREE.MeshLambertMaterial({color: 0xffffff});
+var MeshClickColor = new THREE.MeshLambertMaterial({color: 0xff0000});
 var menuClick = document.getElementById('menu');
 var loader = new THREE.STLLoader();
 //zbelt 
 loader.load('models/zBelts.stl', function(object) {      //file location
-    object.traverse(function(child) {
-        if (child instanceof THREE.Mesh)
-            child.material.color.setHex(baseColor);
-    });
-    scene.add(object);
+  			var mesh = new THREE.mesh(object, meshBaseColor);
+    scene.add(mesh);
+}
 
     var thing = document.getElementById('zbelt');         //get id of associate menu item
     menuClick.addEventListener('click', function() {
         if (event.target == thing) {
-            object.traverse(function(child) {
+            mesh.traverse(function(child) {
                 if (child.material) {
-                    child.material.color.setHex(0xff0000);
+                    child.material.color.MeshClickColor;
                     child.material.opacity = 1;
                     child.material.transparent = true;
                 }
@@ -22,7 +22,7 @@ loader.load('models/zBelts.stl', function(object) {      //file location
         } else {
             object.traverse(function(child) {
                 if (child.material) {
-                    child.material.color.setHex(0xffffff);
+                    child.material.color.meshBaseColor;
                     child.material.opacity = .4;
                     child.material.transparent = true;
                 }
@@ -31,62 +31,100 @@ loader.load('models/zBelts.stl', function(object) {      //file location
     });
 });
 
-//zmotor
-loader.load('models/zMotor.stl', function(object) {      //file location
-    object.traverse(function(child) {
-        if (child instanceof THREE.Mesh)
-            child.material.color.setHex(baseColor);
-    });
-    scene.add(object);
 
-    var thing = document.getElementById('zmotor');         //get id of associate menu item
-    menuClick.addEventListener('click', function() {
-        if (event.target == thing) {
-            object.traverse(function(child) {
-                if (child.material) {
-                    child.material.color.setHex(0xff0000);
-                     child.material.opacity = 1;
-                    child.material.transparent = true;
-                }
-            });
-        } else {
-            object.traverse(function(child) {
-                if (child.material) {
-                    child.material.color.setHex(0xffffff);
-                    child.material.opacity = .4;
-                    child.material.transparent = true;
-                }
-            });
-        }
-    });
-});
 
-//zpulley
-loader.load('models/zScrew.stl', function(object) {      //file location
-    object.traverse(function(child) {
-        if (child instanceof THREE.Mesh)
-            child.material.color.setHex(baseColor);
-    });
-    scene.add(object);
 
-    var thing = document.getElementById('zscrews');         //get id of associate menu item
-    menuClick.addEventListener('click', function() {
-        if (event.target == thing) {
-            object.traverse(function(child) {
-                if (child.material) {
-                    child.material.color.setHex(0xff0000);
-                    child.material.opacity = 1;
-                    child.material.transparent = true;
-                }
-            });
-        } else {
-            object.traverse(function(child) {
-                if (child.material) {
-                    child.material.color.setHex(0xffffff);
-                    child.material.opacity = .4;
-                    child.material.transparent = true;
-                }
-            });
-        }
-    });
-});
+
+
+// var baseColor = 0xffffff;
+// var menuClick = document.getElementById('menu');
+// var loader = new THREE.STLLoader();
+// //zbelt 
+// loader.load('models/zBelts.stl', function(object) {      //file location
+//     object.traverse(function(child) {
+//         if (child instanceof THREE.Mesh)
+//             child.material.color.setHex(baseColor);
+//     });
+//     scene.add(object);
+
+//     var thing = document.getElementById('zbelt');         //get id of associate menu item
+//     menuClick.addEventListener('click', function() {
+//         if (event.target == thing) {
+//             object.traverse(function(child) {
+//                 if (child.material) {
+//                     child.material.color.setHex(0xff0000);
+//                     child.material.opacity = 1;
+//                     child.material.transparent = true;
+//                 }
+//             });
+//         } else {
+//             object.traverse(function(child) {
+//                 if (child.material) {
+//                     child.material.color.setHex(0xffffff);
+//                     child.material.opacity = .4;
+//                     child.material.transparent = true;
+//                 }
+//             });
+//         }
+//     });
+// });
+
+// //zmotor
+// loader.load('models/zMotor.stl', function(object) {      //file location
+//     object.traverse(function(child) {
+//         if (child instanceof THREE.Mesh)
+//             child.material.color.setHex(baseColor);
+//     });
+//     scene.add(object);
+
+//     var thing = document.getElementById('zmotor');         //get id of associate menu item
+//     menuClick.addEventListener('click', function() {
+//         if (event.target == thing) {
+//             object.traverse(function(child) {
+//                 if (child.material) {
+//                     child.material.color.setHex(0xff0000);
+//                      child.material.opacity = 1;
+//                     child.material.transparent = true;
+//                 }
+//             });
+//         } else {
+//             object.traverse(function(child) {
+//                 if (child.material) {
+//                     child.material.color.setHex(0xffffff);
+//                     child.material.opacity = .4;
+//                     child.material.transparent = true;
+//                 }
+//             });
+//         }
+//     });
+// });
+
+// //zpulley
+// loader.load('models/zScrew.stl', function(object) {      //file location
+//     object.traverse(function(child) {
+//         if (child instanceof THREE.Mesh)
+//             child.material.color.setHex(baseColor);
+//     });
+//     scene.add(object);
+
+//     var thing = document.getElementById('zscrews');         //get id of associate menu item
+//     menuClick.addEventListener('click', function() {
+//         if (event.target == thing) {
+//             object.traverse(function(child) {
+//                 if (child.material) {
+//                     child.material.color.setHex(0xff0000);
+//                     child.material.opacity = 1;
+//                     child.material.transparent = true;
+//                 }
+//             });
+//         } else {
+//             object.traverse(function(child) {
+//                 if (child.material) {
+//                     child.material.color.setHex(0xffffff);
+//                     child.material.opacity = .4;
+//                     child.material.transparent = true;
+//                 }
+//             });
+//         }
+//     });
+// });
