aa81fc92f36af18476a40575e6d3947e6b4025f3
zhinii <hansen.c.ralph@gmail.com>
2017-03-20 18:20:29 -0400

    Update laserparts.js

--- laser/js/laserparts.js
+++ laser/js/laserparts.js
@@ -62,3 +62,31 @@ else{
         }
 });
 });
+
+
+//zscrew
+
+objloader.load('objmodels/zscrew.obj', function(object){      //change location
+    scene.add(object);
+
+menuClick.addEventListener('click', function(){
+if (event.target.id === 'zscrews'){                                        //change id of clicked object
+            object.traverse(function(child) {
+                if (child.material) {
+                    child.material.color.setHex(0xff0000);   //change color of selected object
+                    child.material.opacity = 1;
+                    child.material.transparent = true;
+                }
+            });
+        }
+else{
+           object.traverse(function(child) {
+                if (child.material) {
+                    child.material.color.setHex(0xffffff);  //change color if not selected
+                    child.material.opacity = .7;
+                    child.material.transparent = true;
+                }
+            });
+        }
+});
+});
