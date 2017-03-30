f793930efbf4d9099704f99ad076739eb95c41b7
zhinii <hansen.c.ralph@gmail.com>
2017-03-30 16:19:36 -0400

    Update images.js

--- laser/js/images.js
+++ laser/js/images.js
@@ -9,10 +9,10 @@ menuClick.addEventListener('click', function(){
      var images = document.querySelectorAll("#imageshow img");
       for(var i = 0; i < images.length; i++){
     if(event.target.id === 'zmotor'){  
-      images[i].src = images[3].getAttribute('data-src');
+      images.src = images[3].getAttribute('data-src');
       }
       else if(event.target.id === 'xmotor'){
-        images[i].src = images[2].getAttribute('data-src');
+        images.src = images[2].getAttribute('data-src');
       }
   }
 })
