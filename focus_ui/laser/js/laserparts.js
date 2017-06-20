58fdc2afb41f9dd9908c73c0cba088e257f641bb
zhinii <hansen.c.ralph@gmail.com>
2017-06-20 12:30:02 -0400

    Add files via upload

--- focus_ui/laser/js/laserparts.js
+++ focus_ui/laser/js/laserparts.js
@@ -6,19 +6,6 @@ var objloader = new THREE.OBJLoader();
 var stlloader = new THREE.STLLoader();
 
 
-//xmotor
-
-objloader.load('objmodels/xmotor.obj', function(object){      //*******change location
-    scene.add(object);
-
-});
-
-//xbelt
-
-objloader.load('objmodels/xbelt.obj', function(object){      //*******change location
-    scene.add(object);
-});
-
 //xrail
 
 objloader.load('objmodels/xrail.obj', function(object){      //*******change location
@@ -50,28 +37,10 @@ objloader.load('objmodels/platform.obj', function(object){      //*******change 
 
 });
 
-//honeycomb
-
-objloader.load('objmodels/honeycomb.obj', function(object){      //*******change location
-    scene.add(object);
-
-});
-
-//mirror
-
-objloader.load('objmodels/mirror.obj', function(object){      //*******change location
-    scene.add(object);
-
-});
 
 
 
-//carriage
 
-objloader.load('objmodels/carriage.obj', function(object){      //*******change location
-    scene.add(object);
-
-});
 
 //controlpad
 
@@ -79,8 +48,3 @@ objloader.load('objmodels/controlpad.obj', function(object){      //*******chang
     scene.add(object);
 });
 
-//lensmirror
-
-objloader.load('objmodels/lensmirror.obj', function(object){      //*******change location
-    scene.add(object);
-});
