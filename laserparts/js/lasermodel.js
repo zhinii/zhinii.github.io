3f5faf6ac7834c2e7f9f38c433d44514c0699a30
fablabgsd <applytofablab@gsd.harvard.edu>
2017-09-17 19:42:21 -0400

    raycast test

--- laserparts/js/lasermodel.js
+++ laserparts/js/lasermodel.js
@@ -37,18 +37,61 @@ renderer.setClearColor( 0xCCCCCC );
 				loader.setMaterials(material);
 				loader.load('models/chassis.obj', function(object){
 					scene.add(object);
-				
+          //push object to array
+				  stuff.push(object);
 				});
 				
 			});
 
-//load obj with no mtl and set material manually
+//load obj with no mtl and material is default
 
 objloader.load('models/zScrew.obj', function(object){      //change location
     scene.add(object);
 
 });
 
+
+
+//ray casting with mouse picking objects
+
+   var baseColor = 0xffffff;
+    var intersectColor = 0x00D66B;
+    var raycaster = new THREE.Raycaster();
+    var mouse = new THREE.Vector2();
+    var intersected;
+    //an array for raycasting, push objects into it. we will call array later
+    var stuff = [];
+
+     function onDocumentClick(event) { // This is a function to run when we click; we get information about the event through the `event` parameter
+        
+        // Set the x and y coordinates of our mouse vector to our pointer position, scaling for the width and height of our renderer (400 and 300, respectively)
+        var offsetX = renderer.domElement.offsetLeft*(2/sceneWidth);
+        var offsetY = renderer.domElement.offsetTop*(2/sceneHeight);
+        mouse.x = (event.clientX / sceneWidth) * 2 - 1 - offsetX;
+        mouse.y = -(event.clientY / sceneHeight) * 2 + 1 +offsetY;
+        
+        // Tell our raycaster to cast from our mouse
+        raycaster.setFromCamera(mouse, camera);
+        
+        // Tell our raycaster to detect the intersection with that array of objects
+        var intersections = raycaster.intersectObjects(objects, true);
+        if (intersections.length > 0) { 
+        // If we find any intersections
+          // Do stuff  if (intersected != intersections[0].object) {
+                if (intersected) intersected.material.color.setHex(baseColor);
+                intersected = intersections[0].object;
+                intersected.material.color.setHex(intersectColor);
+            }
+            document.body.style.cursor = 'pointer';
+        } else if (intersected) {
+            intersected.material.color.setHex(baseColor);
+            intersected = null;
+            document.body.style.cursor = 'auto';
+        }
+   
+        }
+         
+    };
 
 	
 		
