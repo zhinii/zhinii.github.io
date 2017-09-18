bc6795e577d0e061c8d4f9bcaae5f6e053b745e4
fablabgsd <applytofablab@gsd.harvard.edu>
2017-09-18 12:35:57 -0400

    added parts

--- laserparts/js/lasermodel.js
+++ laserparts/js/lasermodel.js
@@ -58,6 +58,141 @@ objloader.load('models/zScrew.obj', function(object){
           stuff.push(object);
 });
 
+objloader.load('models/zMotor.obj', function(object){  
+  //set material
+    object.traverse( function ( child ) {
+        if ( child instanceof THREE.Mesh ) {
+              child.material.color.setHex(0xff0000);
+            // child.material = material;
+        }
+    });
+      //change location
+    scene.add(object);
+     //push object to array
+          stuff.push(object);
+});
+
+
+objloader.load('models/zBelt.obj', function(object){  
+  //set material
+    object.traverse( function ( child ) {
+        if ( child instanceof THREE.Mesh ) {
+              child.material.color.setHex(0xff0000);
+            // child.material = material;
+        }
+    });
+      //change location
+    scene.add(object);
+     //push object to array
+          stuff.push(object);
+});
+
+objloader.load('models/yMotor.obj', function(object){  
+  //set material
+    object.traverse( function ( child ) {
+        if ( child instanceof THREE.Mesh ) {
+              child.material.color.setHex(0xff0000);
+            // child.material = material;
+        }
+    });
+      //change location
+    scene.add(object);
+     //push object to array
+          stuff.push(object);
+});
+
+objloader.load('models/yBelt.obj', function(object){  
+  //set material
+    object.traverse( function ( child ) {
+        if ( child instanceof THREE.Mesh ) {
+              child.material.color.setHex(0xff0000);
+            // child.material = material;
+        }
+    });
+      //change location
+    scene.add(object);
+     //push object to array
+          stuff.push(object);
+});
+objloader.load('models/xMotor.obj', function(object){  
+  //set material
+    object.traverse( function ( child ) {
+        if ( child instanceof THREE.Mesh ) {
+              child.material.color.setHex(0xff0000);
+            // child.material = material;
+        }
+    });
+      //change location
+    scene.add(object);
+     //push object to array
+          stuff.push(object);
+});
+objloader.load('models/pad.obj', function(object){  
+  //set material
+    object.traverse( function ( child ) {
+        if ( child instanceof THREE.Mesh ) {
+              child.material.color.setHex(0xff0000);
+            // child.material = material;
+        }
+    });
+      //change location
+    scene.add(object);
+     //push object to array
+          stuff.push(object);
+});
+objloader.load('models/mirrorTwo.obj', function(object){  
+  //set material
+    object.traverse( function ( child ) {
+        if ( child instanceof THREE.Mesh ) {
+              child.material.color.setHex(0xff0000);
+            // child.material = material;
+        }
+    });
+      //change location
+    scene.add(object);
+     //push object to array
+          stuff.push(object);
+});
+
+objloader.load('models/mirrorLens.obj', function(object){  
+  //set material
+    object.traverse( function ( child ) {
+        if ( child instanceof THREE.Mesh ) {
+              child.material.color.setHex(0xff0000);
+            // child.material = material;
+        }
+    });
+      //change location
+    scene.add(object);
+     //push object to array
+          stuff.push(object);
+});
+objloader.load('models/honeycomb.obj', function(object){  
+  //set material
+    object.traverse( function ( child ) {
+        if ( child instanceof THREE.Mesh ) {
+              child.material.color.setHex(0xff0000);
+            // child.material = material;
+        }
+    });
+      //change location
+    scene.add(object);
+     //push object to array
+          stuff.push(object);
+});
+objloader.load('models/doorSensor.obj', function(object){  
+  //set material
+    object.traverse( function ( child ) {
+        if ( child instanceof THREE.Mesh ) {
+              child.material.color.setHex(0xff0000);
+            // child.material = material;
+        }
+    });
+      //change location
+    scene.add(object);
+     //push object to array
+          stuff.push(object);
+});
 
 
 //ray casting with mouse picking objects
