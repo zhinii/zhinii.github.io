725a643b05bd7a3b5d91a8a23fc25ced463b3e4d
fablabgsd <applytofablab@gsd.harvard.edu>
2017-11-05 17:27:47 -0500

    pick and drop

---
+++ 2 player template/Assets/MoveObject.cs
@@ -0,0 +1,38 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class MoveObject : MonoBehaviour {
+
+// 	public GameObject item; //ibjecr we want to grab
+// 	public GameObject tempParent;  //object we want to attached it to when picked up until its dropped
+// 	public Transform guide;  //position we want it to sit when picked up
+// 	public Transform drop; //where the object will go when dropped.
+// 	// Use this for initialization
+// 	void Start () {
+// 		item.GetComponent<Rigidbody>().useGravity = true;
+// 	}
+	
+// 	// Update is called once per frame
+// 	void Update () {
+// 		//picks up ball
+// 		if (Input.GetKeyDown("space")){
+//            item.GetComponent<Rigidbody>().useGravity = false;
+// 		item.GetComponent<Rigidbody>().isKinematic = true;
+// 		item.transform.position = guide.transform.position;
+// 		item.transform.rotation = guide.transform.rotation;
+// 		item.transform.parent = tempParent.transform;
+// 	}
+// 	///drops ball
+// 	  if (Input.GetKeyDown("return")){
+// 	  item.GetComponent<Rigidbody>().useGravity = true;
+// 		item.GetComponent<Rigidbody>().isKinematic = false;
+// 		item.transform.parent = null;
+// 		item.transform.position = drop.transform.position;
+// 	}
+// 	}
+
+	
+
+
+// }
---
+++ 2 player template/Assets/hitMe.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hitMe : MonoBehaviour {
+
+		public GameObject item; //ibjecr we want to grab
+	public GameObject tempParent;  //object we want to attached it to when picked up until its dropped
+	public Transform guide;  //position we want it to sit when picked up
+	public Transform drop; //where the object will go when dropped.
+
+	bool playerInTrigger; ///this is used to indicate player is in 'hit zone'
+
+//this detect if player 1 is in 'hit area' to pick up object
+	void OnTriggerEnter (Collider col)
+	{
+		if(col.gameObject.name == "player1")
+	{
+			Debug.Log("hit");
+
+		playerInTrigger = true;
+	
+	}
+
+}
+
+//this says if player is in 'hit area' and they press the right key, they will pick up object
+	void Update(){
+		if(playerInTrigger && Input.GetKeyDown("space")){
+           item.GetComponent<Rigidbody>().useGravity = false;
+		item.GetComponent<Rigidbody>().isKinematic = true;
+		item.transform.position = guide.transform.position;
+		item.transform.rotation = guide.transform.rotation;
+		item.transform.parent = tempParent.transform;
+	}
+	}
+}
