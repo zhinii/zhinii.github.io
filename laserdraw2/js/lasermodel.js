bf13577498229321601dcb07dc3147c696086b96
zhinii <hansen.c.ralph@gmail.com>
2018-03-19 18:09:15 -0400

    Add files via upload

---
+++ laserdraw2/js/lasermodel.js
@@ -0,0 +1,165 @@
+var clickToHide = document.getElementsByClassName('accordion');
+for(var i = 0; i <clickToHide.length; i++){
+clickToHide[i].addEventListener('click', function(){
+	console.log(i);
+});
+}
+
+
+
+
+
+var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 500);
+    var renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+renderer.setClearColor( 0xCCCCCC );
+  camera.position.z = 20;
+      camera.position.y = 70;
+  	    camera.position.x = 0;
+    renderer.setSize(window.innerWidth-10, window.innerHeight-10);
+    document.body.appendChild(renderer.domElement);
+    renderer.domElement.id = "context"
+    scene.add(new THREE.HemisphereLight(0xaaaaaa, 0x444444));
+    var light = new THREE.DirectionalLight(0xffffff, 0.5);
+    light.position.set(1, 1, 1);
+    scene.add(light);
+			
+			var lasermodelClear = new THREE.MTLLoader();
+			
+		lasermodelClear.load('models/plsClear.mtl', function(material){
+				material.preload();
+				var loader = new THREE.OBJLoader();
+				loader.setMaterials(material);
+				loader.load('models/plsClear.obj', function(object){
+					scene.add(object);
+for(var i = 0; i <clickToHide.length; i++){
+			clickToHide[i].addEventListener('click', function(){
+	object.visible = false;
+
+});
+}				
+				});
+				
+			});
+
+			var lasermodelWire = new THREE.MTLLoader();
+			
+		lasermodelWire.load('models/wireframe.mtl', function(material){
+				material.preload();
+				var loader = new THREE.OBJLoader();
+				loader.setMaterials(material);
+				loader.load('models/wireframe.obj', function(object){
+					scene.add(object);
+					object.visible = false;
+
+for(var i = 0; i <clickToHide.length; i++){
+			clickToHide[i].addEventListener('click', function(){
+					object.visible = true;
+
+});
+}				
+				});
+				
+			});
+
+
+		var lasermodelSolid = new THREE.MTLLoader();
+			
+			lasermodelSolid.load('models/plsSolid.mtl', function(material){
+				material.preload();
+				var loader = new THREE.OBJLoader();
+				loader.setMaterials(material);
+				loader.load('models/plsSolid.obj', function(object){
+					scene.add(object);
+				
+				});
+				
+			});
+
+
+	
+		
+			
+			  var render = function() {
+	    
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
+        controls.update();
+    };
+    ///scene controls for mouse
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+	    controls.target.set( 0,30,0 );
+    render();
+    //end of controls
+
+
+       //thoughts to draw on scene
+       // capture scene as image and draw to canvas
+       //draw on canvas with pen
+       //save drawn on image as download
+       //to view in 3d, clear canvas
+
+
+       ///below is 2d canvas
+        //assign canvas to var
+                var canvas = document.getElementById('artboard');
+                canvas.width = window.innerWidth;
+                canvas.height = window.innerHeight;
+                //create drawing elements in canvas i.e. line color, width
+                canvas.style.display = 'none';
+                var ctx = canvas.getContext('2d');
+                ctx.strokeStyle = '#ff6600';
+                ctx.lineWidth = 2;
+                // var for mouse down or up
+                var isMousedown = false;
+                //empty var for point location
+                var lastPoint = {
+                    x: null,
+                    y: null
+                };
+                //create eventlistener for mousdown and tracks the coordinates of this event
+                canvas.addEventListener('mousedown', function(event) {
+                    isMousedown = true;
+                    lastPoint.x = event.x;
+                    lastPoint.y = event.y;
+                });
+                //event listener for mouse up
+                canvas.addEventListener('mouseup', function() {
+                    isMousedown = false;
+                });
+                //event listener for mouse move and drawing on the move
+                canvas.addEventListener('mousemove', function(event) {
+                    //if mousdown is true
+                    if (isMousedown) {
+                        console.log(event.x, event.y);
+                        //use those coordinate to draw a line
+                        ctx.beginPath();
+                        ctx.moveTo(lastPoint.x, lastPoint.y);
+                        ctx.lineTo(event.x, event.y);
+                        ctx.closePath();
+                        ctx.stroke();
+                        //create and place coordinate of each 'stroke'
+                        lastPoint.x = event.x;
+                        lastPoint.y = event.y;
+                    }
+                });
+
+       var draw = document.getElementById('draw2d');
+       var nav = document.getElementById('view3d');
+
+       draw.addEventListener('click', function(){
+
+                 ctx.clearRect(0, 0, canvas.width, canvas.height);
+                  	  render();
+        canvas.style.display = 'block';
+         // render();
+       });
+
+       nav.addEventListener('click', function(){
+        canvas.style.display = 'none';
+        console.log('hide');
+       });
+
+
---
+++ laserdraw2/js/laserparts.js
@@ -0,0 +1,176 @@
+
+
+var meshBaseColor = new THREE.MeshLambertMaterial({color: 0xffffff});
+var MeshClickColor = new THREE.MeshLambertMaterial({color: 0xff0000});
+
+   var baseColor = 0xff0000;
+    var intersectColor = 0x00D66B;
+var sceneWidth = window.innerWidth-10;
+var sceneHeight = window.innerHeight-10;
+
+var objloader = new THREE.OBJLoader();
+var stlloader = new THREE.STLLoader();
+
+
+
+objloader.load('models/zbelt.obj', function(object){
+    scene.add(object);
+    stuff.push(object);
+});
+
+
+//zmotor
+
+objloader.load('models/zmotor.obj', function(object){
+    scene.add(object);
+    stuff.push(object);
+    motorZ.push(object);
+});
+
+
+//zscrew
+
+objloader.load('models/zscrew.obj', function(object){      //change location
+    scene.add(object);
+    stuff.push(object);
+});
+
+
+
+//xmotor
+
+objloader.load('models/xmotor.obj', function(object){      //*******change location
+    scene.add(object);
+    stuff.push(object);
+    motorX.push(object);
+});
+
+//xbelt
+
+objloader.load('models/xbelt.obj', function(object){      //*******change location
+    scene.add(object);
+    stuff.push(object);
+});
+
+//xrail
+
+objloader.load('models/xrail.obj', function(object){      //*******change location
+    scene.add(object);
+    stuff.push(object);
+});
+
+//ymotor
+
+objloader.load('models/ymotor.obj', function(object){      //*******change location
+    scene.add(object);
+    stuff.push(object);
+});
+
+//yrails
+
+objloader.load('models/yrail.obj', function(object){      //*******change location
+    scene.add(object);
+    stuff.push(object);
+
+});
+
+//ybelt
+
+objloader.load('models/ybelt.obj', function(object){      //*******change location
+    scene.add(object);
+    stuff.push(object);
+});
+
+
+//rulers
+
+objloader.load('models/rulers.obj', function(object){      //*******change location
+    scene.add(object);
+    stuff.push(object);
+});
+
+//platform
+
+objloader.load('models/platform.obj', function(object){      //*******change location
+    scene.add(object);
+    stuff.push(object);
+});
+
+//honeycomb
+
+objloader.load('models/honeycomb.obj', function(object){      //*******change location
+    scene.add(object);
+    stuff.push(object);
+});
+
+//mirror
+
+objloader.load('models/mirror.obj', function(object){      //*******change location
+    scene.add(object);
+    stuff.push(object);
+    
+});
+
+//lens
+
+objloader.load('models/lensmirror.obj', function(object){      //*******change location
+    scene.add(object);
+    stuff.push(object);
+  
+
+});
+
+//carriage
+
+objloader.load('models/carriage.obj', function(object){      //*******change location
+    scene.add(object);
+    stuff.push(object);
+});
+
+//controlpad
+
+objloader.load('models/controlpad.obj', function(object){      //*******change location
+    scene.add(object);
+    stuff.push(object);
+});
+
+
+var stuff = [];
+   var raycaster = new THREE.Raycaster();
+    var mouse = new THREE.Vector2();
+    var intersected;
+
+    //below is raycaster for mouse move intersections, second raycaster function is needed for click events
+     function onDocumentMouseMove(event) { // This is a function to run when we click; we get information about the event through the `event` parameter
+        
+        // Set the x and y coordinates of our mouse vector to our pointer position, scaling for the width and height of our renderer (400 and 300, respectively)
+        var offsetX = renderer.domElement.offsetLeft*(2/sceneWidth);
+        var offsetY = renderer.domElement.offsetTop*(2/sceneHeight);
+        mouse.x = (event.clientX / sceneWidth) * 2 - 1 - offsetX;
+        mouse.y = -(event.clientY / sceneHeight) * 2 + 1 +offsetY;
+        
+        // Tell our raycaster to cast from our mouse
+        raycaster.setFromCamera(mouse, camera);
+        
+        // Tell our raycaster to detect the intersection with that array of objects
+        var intersections = raycaster.intersectObjects(stuff, true);
+        if (intersections.length > 0) { 
+        // If we find any intersections
+          // Do stuff  
+          if (intersected != intersections[0].object) {
+                if (intersected) intersected.material.color.setHex(intersected.currentHex);
+                intersected = intersections[0].object;
+                intersected.currentHex = intersected.material.color.getHex();
+                intersected.material.color.setHex(intersectColor);
+            }
+            document.body.style.cursor = 'pointer';
+        } else if (intersected) {
+            intersected.material.color.setHex(intersected.currentHex);
+            intersected = null;
+            document.body.style.cursor = 'auto';
+        }
+   
+        };
+
+        //attaches event listender to the renderer, which is a dom element. so when a mouse moves it is listening and executing our function for raycasting
+     renderer.domElement.addEventListener('mousemove', onDocumentMouseMove, false);
